7cc69dab4bee4370c033e737af6efe57f502e7d5
Friyn <friyn@users.noreply.github.com>
2025-09-27 11:36:44 +0700

    login and register page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(color: Colors.white),
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/bg_overlay_white.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          // Center(
+          //   child: Image(image: AssetImage('assets/images/masuk.png'), width: 247, height: 247,),
+          // ),
+          Container(
+            margin: const EdgeInsets.all(50),
+            child: Column(
+              children: [
+                Image(image: AssetImage('assets/images/masuk.png'), width: 247,),
+                const SizedBox(height: 30),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: const Color(0xFF0056b3).withValues(alpha: 0.1),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide.none,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(
+                              color: Colors.transparent,
+                              width: 0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(
+                              color: Color(0xFF0056b3),
+                              width: 2,
+                            ),
+                          ),
+                          labelText: 'Email',
+                          labelStyle: GoogleFonts.poppins(
+                            fontSize: 11,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF333333),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: const Color(0xFF0056b3).withValues(alpha: 0.1),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide.none,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(
+                              color: Colors.transparent,
+                              width: 0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(
+                              color: Color(0xFF0056b3),
+                              width: 2,
+                            ),
+                          ),
+                          labelText: 'Password',
+                          labelStyle: GoogleFonts.poppins(
+                            fontSize: 11,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF333333),
+                          ),
+                          suffixIcon: Icon(Icons.visibility_off, color: Color(0xFF333333)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Lupa Password?',
+                      style: GoogleFonts.poppins(
+                        fontSize: 11,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF0056B3),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                SizedBox(
+                  height: 43,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF0056B3),
+                      minimumSize: const Size(0, 48), // tinggi tombol
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Masuk',
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 11),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const SizedBox(width: 12),
+                      Text(
+                        'Belum Punya Akun?',
+                        style: GoogleFonts.poppins(
+                          fontSize: 11,
+                          fontWeight: FontWeight.w400,
+                          color: const Color(0xFF1F1A1A).withValues(alpha: 0.9),
+                        ),
+                      ),
+                      const SizedBox(width: 5),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          minimumSize: Size(0, 0),
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'Daftar',
+                          style: GoogleFonts.poppins(
+                            fontSize: 11,
+                            fontWeight: FontWeight.w500,
+                            color: const Color(0xFF0056B3),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ),
+                const SizedBox(height: 30),
+                Text('Atau', 
+                  style: GoogleFonts.poppins(
+                    fontSize: 11,
+                    // letterSpacing: 4,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xFF0056B3),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  height: 43,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF0056b3).withValues(alpha: 0.1),
+                      elevation: 0,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      foregroundColor: const Color(0xFF333333),
+                      minimumSize: const Size(0, 48),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/google.png',
+                          width: 18,
+                          height: 18,
+                        ),
+                        const SizedBox(width: 20),
+                        Text(
+                          'Masuk Dengan Google',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
