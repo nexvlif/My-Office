698a2c8ad9ba83ac8103181143f4735ce5f3b241
Nex <nexvlif@gmail.com>
2025-09-27 11:35:09 +0700

    Add Initialize database

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:myoffice/pages/splash_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  late final supabase_url = dotenv.env['SUPABASE_URL']!;
+  late final supabase_anon_key = dotenv.env['YOUR_SUPABASE_PUBLISHABLE_KEY']!;
+
+  await Supabase.initialize(
+    url: supabase_url,
+    anonKey: supabase_anon_key,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:myoffice/pages/home_page.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
